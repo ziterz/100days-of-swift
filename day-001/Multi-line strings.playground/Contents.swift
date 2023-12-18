ac9d26e79918b3d596851dfbce83eed7c5b58f08
Ziterz <ziady.mubaraq@gmail.com>
2023-12-18 22:23:29 +0700

    learn: day 1 about multi-line string

---
+++ day-001/Multi-line strings.playground/Contents.swift
@@ -0,0 +1,17 @@
+// Created by Ziady Mubaraq on 18/12/23.
+
+import UIKit
+
+//Multi-line strings with line breaks.
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// Multi-line string without line breaks.
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
