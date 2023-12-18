9b9d3e26334b51f1f6e0aa9e4e0c40208343ae64
Ziterz <ziady.mubaraq@gmail.com>
2023-12-18 22:13:41 +0700

    learn: day 1 about variable

---
+++ day-001/Variables.playground/Contents.swift
@@ -0,0 +1,8 @@
+// Created by Ziady Mubaraq on 18/12/23.
+
+import UIKit
+
+var greeting = "Hello from 2023"
+greeting = "Goodbye from 2024"
+
+// they modify the existing value rather than creating a new variable.
