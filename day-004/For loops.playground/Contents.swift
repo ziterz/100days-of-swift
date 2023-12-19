8633229a7d47b051a26c229b7ee159c443096fe4
Ziterz <ziady.mubaraq@gmail.com>
2023-12-20 00:02:15 +0700

    learn: day 4 about for loops

---
+++ day-004/For loops.playground/Contents.swift
@@ -0,0 +1,39 @@
+// Created by Ziady Mubaraq on 19/12/23.
+
+import UIKit
+
+let count = 1...10
+
+for number in count {
+  print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+  print("\(album) is on Apple Music")
+}
+
+print("Players gonna ")
+
+for _ in 1...5 {
+  print("play")
+}
+
+let names = ["Sterling", "Cyril", "Lana", "Ray", "Pam"]
+
+for name in names {
+  print("\(name) is a secret agent")
+}
+
+let names2 = ["Sterling", "Cyril", "Lana", "Ray", "Pam"]
+
+for name in names2 {
+    print("\(name) is a secret agent")
+}
+
+let names3 = ["Sterling", "Cyril", "Lana", "Ray", "Pam"]
+
+for _ in names3 {
+    print("[CENSORED] is a secret agent!")
+}
