cb224656daa991ed45a82d8bcd9698d3d468b898
Ziterz <ziady.mubaraq@gmail.com>
2023-12-21 15:43:25 +0700

    learn: day 4 about infinity loops

---
+++ day-004/Infinite loops.playground/Contents.swift
@@ -0,0 +1,28 @@
+// Created by Ziady Mubaraq on 21/12/23.
+
+import UIKit
+
+var counter = 0
+
+while true {
+  print(" ")
+  counter += 1
+
+  if counter == 273 {
+    break
+  }
+}
+
+while true {
+    print("I'm alive!")
+}
+
+print("I've snuffed it!")
+
+var isAlive = false
+
+while isAlive == true {
+    print("I'm alive!")
+}
+
+print("I've snuffed it!")
