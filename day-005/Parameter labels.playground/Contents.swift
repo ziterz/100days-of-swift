487e8ff10da6f96e1d96e130f1394ae5eb9ecd7f
Ziterz <ziady.mubaraq@gmail.com>
2023-12-21 16:47:53 +0700

    learn: day 5 about parameter labels

---
+++ day-005/Parameter labels.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Created by Ziady Mubaraq on 21/12/23.
+
+import UIKit
+
+func square(number: Int) -> Int {
+  return number * number
+}
+
+let result = square(number: 8)
+
+func sayHello(to name: String) {
+  print("Hello, \(name)!")
+}
+
+sayHello(to: "Taylor")
--- day-005/Returning values.playground/Contents.swift
+++ day-005/Returning values.playground/Contents.swift
@@ -32,16 +32,16 @@ greet(name: "Ziady")
 
 func greet2(name: String) -> String {
     if name == "Taylor Swift" {
-        "Oh wow!"
+      "Oh wow!"
     } else {
-        "Hello, \(name)"
+      "Hello, \(name)"
     }
 }
 
 greet2(name: "Ziady")
 
 func greet3(name: String) -> String {
-    name == "Taylor Swift" ? "Oh wow!" : "Hello, \(name)"
+  name == "Taylor Swift" ? "Oh wow!" : "Hello, \(name)"
 }
 
 greet3(name: "Ziady")
@@ -56,7 +56,7 @@ print(user[0])
 
 // A second pass at the function might look like this
 func getUser2() -> [String: String] {
-    ["first": "Taylor", "last": "Swift"]
+  ["first": "Taylor", "last": "Swift"]
 }
 
 let user2 = getUser2()
@@ -64,7 +64,7 @@ print(user2["first", default: "unknown"])
 
 // much more safely
 func getUser3() -> (first: String, last: String) {
-    (first: "Taylor", last: "Swift")
+  (first: "Taylor", last: "Swift")
 }
 
 let user3 = getUser3()
