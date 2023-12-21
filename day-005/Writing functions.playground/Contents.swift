bdce8f8df4350326e429900e76f7ccbf777584a7
Ziterz <ziady.mubaraq@gmail.com>
2023-12-21 16:06:19 +0700

    learn: day 4 is complete, day 5 about writing a function

---
+++ day-005/Writing functions.playground/Contents.swift
@@ -0,0 +1,16 @@
+// Created by Ziady Mubaraq on 21/12/23.
+
+import UIKit
+
+func printHelp() {
+  let message = """
+  Welcome to MyApp!
+
+  Run this app inside a directory of images and
+  MyApp will resize them all into thumbnails
+  """
+
+  print(message)
+}
+
+printHelp()
