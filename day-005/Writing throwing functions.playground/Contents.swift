679e2f1f0d81e68d6f2c304ae18fb9918007c449
Ziterz <ziady.mubaraq@gmail.com>
2023-12-21 20:15:10 +0700

    learn: day 5 about writing throwing function

---
+++ day-005/Writing throwing functions.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Created by Ziady Mubaraq on 21/12/23.
+
+import UIKit
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+  if password == "password" {
+    throw PasswordError.obvious
+  }
+  
+  return true
+}
