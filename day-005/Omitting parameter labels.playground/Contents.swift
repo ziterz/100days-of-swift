d7a5005090cd62a11b3e23379b744a642f6b308d
Ziterz <ziady.mubaraq@gmail.com>
2023-12-21 16:55:09 +0700

    learn: day 5 about omitting parameter labels

---
+++ day-005/Omitting parameter labels.playground/Contents.swift
@@ -0,0 +1,9 @@
+// Created by Ziady Mubaraq on 21/12/23.
+
+import UIKit
+
+func greet(_ person: String) {
+  print("Hello, \(person)!")
+}
+
+greet("Taylor")
--- day-005/Parameter labels.playground/Contents.swift
+++ day-005/Parameter labels.playground/Contents.swift
@@ -13,3 +13,21 @@ func sayHello(to name: String) {
 }
 
 sayHello(to: "Taylor")
+
+func setReactorStatus(primaryActive: Bool, backupActive: Bool, isEmergency: Bool) {
+    // code here
+}
+
+setReactorStatus(primaryActive: true, backupActive: true, isEmergency: false)
+
+func setAge(for person: String, to value: Int) {
+  print("\(person) is now \(value)")
+}
+
+setAge(for: "Paul", to: 40)
+
+//setAge(person: "Paul", value: 40)
+
+//func setAge(for: String, to: Int) {
+//    print("\(for) is now \(to)")
+//}
