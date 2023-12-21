ab3a7561b2f2d1cef676466baa3194b629c59fc9
Ziterz <ziady.mubaraq@gmail.com>
2023-12-21 17:04:56 +0700

    learn: day 5 about default parameters

---
+++ day-005/Default parameters.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Created by Ziady Mubaraq on 21/12/23.
+import UIKit
+
+func greet(_ person: String, nicely: Bool = true) {
+  if nicely == true {
+    print("Hello, \(person)!")
+  } else {
+    print("Oh no, it's \(person) again...")
+  }
+}
+
+greet("Taylor")
+greet("Taylor", nicely: false)
+
+func findDirections(from: String, to: String, route: String = "fastest", avoidHighways: Bool = false) {
+    // code here
+}
+
+findDirections(from: "London", to: "Glasgow")
+findDirections(from: "London", to: "Glasgow", route: "scenic")
+findDirections(from: "London", to: "Glasgow", route: "scenic", avoidHighways: true)
