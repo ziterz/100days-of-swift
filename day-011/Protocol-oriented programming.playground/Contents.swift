42cbbe02acfacebb43f553c7d8845e1c34028c94
Ziterz <ziady.mubaraq@gmail.com>
2023-12-27 21:45:09 +0700

    learn: day 11 complete

--- day-011/Protocol-oriented programming.playground/Contents.swift
+++ day-011/Protocol-oriented programming.playground/Contents.swift
@@ -19,6 +19,58 @@ struct User: Identifiable {
 
 let twostraws = User(id: "twostraws")
 twostraws.identify()
+
 /*
  * VALID SYNTAX
  */
+protocol HasAge {
+  var age: Int { get set }
+  mutating func celebrateBirthday()
+}
+
+// -------
+protocol Paintable { }
+protocol Tileable { }
+struct Wall: Paintable, Tileable { }
+
+// -------
+extension Collection {
+  func describe() {
+    if count == 1 {
+      print("There is 1 item.")
+    } else {
+      print("There are \(count) items.")
+    }
+  }
+}
+
+// -------
+protocol Inflatable {
+  mutating func refillAir()
+}
+extension Inflatable {
+  mutating func refillAir() {
+    print("Refilling the air.")
+  }
+}
+
+// -------
+protocol SuitableForKids {
+  var minimumAge: Int { get set }
+  var maximumAge: Int { get set }
+}
+protocol SupportsMultiplePlayers {
+  var minimumPlayers: Int { get set }
+  var maximumPlayers: Int { get set }
+}
+struct FamilyBoardGame: SuitableForKids, SupportsMultiplePlayers {
+  var minimumAge = 3
+  var maximumAge = 110
+  var minimumPlayers = 1
+  var maximumPlayers = 4
+}
+
+// -------
+//protocol Identifiable {
+//  var id: Int { get set }
+//}
