4a2c7c0ac096fbd4049337321dd5030592ed7eac
Ziterz <ziady.mubaraq@gmail.com>
2023-12-28 23:02:56 +0700

    learn: day 12 about force unwrapping

--- day-012/Force unwrapping.playground/Contents.swift
+++ day-012/Force unwrapping.playground/Contents.swift
@@ -42,7 +42,60 @@ enum Direction2: CaseIterable {
 }
 
 let randomDirection2 = Direction2.random()
+
 /*
  * VALID SYNTAX
  */
+let legoBricksSold: Int? = 400_000_000_000
+let numberSold = legoBricksSold!
 
+// -------
+func describe(array: [String]?) {
+  let unwrapped = array!
+  print("The array has \(unwrapped.count) items.")
+}
+describe(array: [])
+
+// -------
+let age: Int = 21
+let allowedMessage: String? = age >= 21 ? "Welcome!" : nil
+let result = allowedMessage!
+
+// -------
+func league(for skillLevel: Int) -> Int? {
+  switch skillLevel {
+  case 1:
+    fallthrough
+  case 2:
+    return 3
+  case 3:
+    return 2
+  case 4:
+    return 1
+  default:
+    return nil
+  }
+}
+let allocatedLeague = league(for: 3)!
+
+// -------
+struct User {
+  var name: String?
+  var age: Int?
+}
+let taylor = User(name: "Taylor", age: 26)
+let taylorAge = taylor.age!
+
+// -------
+func title(for page: Int) -> String? {
+  guard page >= 1 else {
+    return nil
+  }
+  let pageCount = 132
+  if page < pageCount {
+    return "Page \(page)"
+  } else {
+    return nil
+  }
+}
+let pageTitle = title(for: 16)!
