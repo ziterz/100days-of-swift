7ef96d306fe798d331fddd5bbea8b5f8a2db1d61
Ziterz <ziady.mubaraq@gmail.com>
2023-12-29 19:34:58 +0700

    learn: day 13 about types of data

---
+++ day-013/Types of data.playground/Contents.swift
@@ -0,0 +1,58 @@
+// Created by Ziady Mubaraq on  29/12/2023
+
+import UIKit
+
+// MARK: String and Integer
+var name
+name = "Tim McGraw"
+
+var name: String
+name = "Tim McGraw"
+
+var name: String
+name = "Tim McGraw"
+
+var age: Int
+age = 25
+
+name = 25
+age = "Tim McGraw"
+
+// MARK: Float and Double
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+var longitude: Double
+
+// MARK: Boolean
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
+// MARK: Using type annotations wisely
+var name = "Tim McGraw"
+
+var name: String
+name = "Tim McGraw"
+
+var age = 25
+var longitude = -86.783333
+var nothingInBrain = true
+
+var name: String = "Tim McGraw"
