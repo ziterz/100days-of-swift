f088b4132195a59509d519ad780855093571cf74
Ziterz <ziady.mubaraq@gmail.com>
2023-12-29 20:53:26 +0700

    learn: day 13 about dictionaries

---
+++ day-013/Dictionaries.playground/Contents.swift
@@ -0,0 +1,20 @@
+// Created by Ziady Mubaraq on  29/12/2023
+
+import UIKit
+
+var person = ["Taylor", "Alison", "Swift", "December", "taylorswift.com"]
+
+var person = ["first": "Taylor", "middle": "Alison", "last": "Swift", "month": "December", "website": "taylorswift.com"]
+person["middle"]
+person["month"]
+
+var person = [
+  "first": "Taylor",
+  "middle": "Alison",
+  "last": "Swift",
+  "month": "December",
+  "website": "taylorswift.com"
+]
+
+person["middle"]
+person["month"]
