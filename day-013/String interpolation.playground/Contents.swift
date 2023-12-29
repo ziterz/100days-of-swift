8a77edbd51063619488fb2373837fa7084208878
Ziterz <ziady.mubaraq@gmail.com>
2023-12-29 19:50:37 +0700

    learn: day 13 about string interpolation

---
+++ day-013/String interpolation.playground/Contents.swift
@@ -0,0 +1,20 @@
+// Created by Ziady Mubaraq on  29/12/2023
+
+import UIKit
+
+var name = "Tim McGraw"
+
+var name = "Tim McGraw"
+"Your name is \(name)"
+
+var name = "Tim McGraw"
+"Your name is " + name
+
+var name = "Tim McGraw"
+var age = 25
+var latitude = 36.166667
+
+"Your name is \(name), your age is \(age), and your latitude is \(latitude)"
+
+var age = 25
+"You are \(age) years old. In another \(age) years you will be \(age * 2)."
