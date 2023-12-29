21325bb157474bef9ea6a3aa1d8fdc884fde0fc5
Ziterz <ziady.mubaraq@gmail.com>
2023-12-30 01:27:23 +0700

    learn: day 15 about static properties and methods

---
+++ day-015/Static properties and methods.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Created by Ziady Mubaraq on 30/12/2023
+
+import UIKit
+
+struct TaylorFan {
+  static var favoriteSong = "Look What You Made Me Do"
+
+  var name: String
+  var age: Int
+}
+
+let fan = TaylorFan(name: "James", age: 25)
+print(TaylorFan.favoriteSong)
