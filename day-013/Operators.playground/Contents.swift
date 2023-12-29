90e2303a2df81f803c1ee81b8866dd2b2239c5da
Ziterz <ziady.mubaraq@gmail.com>
2023-12-29 19:41:43 +0700

    learn: day 13 about operators

---
+++ day-013/Operators.playground/Contents.swift
@@ -0,0 +1,41 @@
+// Created by Ziady Mubaraq on  29/12/2023
+
+import UIKit
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+// MARK: Comparison operators
+var a = 1.1
+var b = 2.2
+var c = a + b
+
+c > 3
+c >= 3
+c > 4
+c < 4
+
+var name = "Tim McGraw"
+name == "Tim McGraw"
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+var name = "Tim McGraw"
+name == "Tim McGraw"
+name != "Tim McGraw"
