67eb98c603dee35bbbdec0d566d7fb0437568d92
Ziterz <ziady.mubaraq@gmail.com>
2023-12-30 00:35:14 +0700

    learn: day 13 about switch case

---
+++ day-013/Switch case.playground/Contents.swift
@@ -0,0 +1,35 @@
+// Created by Ziady Mubaraq on  30/12/2023
+
+import UIKit
+
+let liveAlbums = 2
+
+switch liveAlbums {
+case 0:
+    print("You're just starting out")
+
+case 1:
+    print("You just released iTunes Live From SoHo")
+
+case 2:
+    print("You just released Speak Now World Tour")
+
+default:
+    print("Have you done something new?")
+}
+
+let studioAlbums = 5
+
+switch studioAlbums {
+case 0...1:
+  fallthrough
+
+case 2...3:
+    print("You're a rising star")
+
+case 4...5:
+    print("You're world famous!")
+
+default:
+    print("Have you done something new?")
+}
