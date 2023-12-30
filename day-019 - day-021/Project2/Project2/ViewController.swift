2ab35d45c83078b2bb21640c4836220751711e26
Ziterz <ziady.mubaraq@gmail.com>
2023-12-31 00:23:35 +0700

    learn: day 19 setting up Project2 for guess the flag

---
+++ day-019 - day-021/Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Ziady Mubaraq on 31/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
