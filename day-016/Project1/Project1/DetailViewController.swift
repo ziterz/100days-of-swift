e475a5e136819b4a4ba83b56e6e2623a8c812008
Ziterz <ziady.mubaraq@gmail.com>
2023-12-30 22:01:55 +0700

    learn: day 17 about loading images with UIImage

--- day-016/Project1/Project1/DetailViewController.swift
+++ day-016/Project1/Project1/DetailViewController.swift
@@ -9,22 +9,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
   @IBOutlet var imageView: UIImageView!
+  var selectedImage: String?
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    if let imageToLoad = selectedImage {
+      imageView.image = UIImage(named: imageToLoad)
     }
-    */
-
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
