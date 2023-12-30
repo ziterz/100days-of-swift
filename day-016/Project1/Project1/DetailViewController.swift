f3eeec0faec7ce45fe1a25c6b68b8c09e99e6821
Ziterz <ziady.mubaraq@gmail.com>
2023-12-30 21:47:00 +0700

    building a detail screen

---
+++ day-016/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Ziady Mubaraq on 30/12/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  @IBOutlet var imageView: UIImageView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
