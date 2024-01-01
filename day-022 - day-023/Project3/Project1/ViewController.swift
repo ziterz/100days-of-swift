6c4f44adfc9414821b112cab8e836e65d0726d52
Ziterz <ziady.mubaraq@gmail.com>
2024-01-01 20:23:20 +0700

    learn: day 22 using UIActivityViewController

---
+++ day-022 - day-023/Project3/Project1/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Ziady Mubaraq on 30/12/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+  // MARK: properties
+  var pictures = [String]()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    title = "Storm Viewer"
+    navigationController?.navigationBar.prefersLargeTitles = true
+    
+    let fm = FileManager.default
+    let path = Bundle.main.resourcePath!
+    let items = try! fm.contentsOfDirectory(atPath: path)
+    
+    for item in items.sorted() {
+      if item.hasPrefix("nssl") {
+        pictures.append(item)
+      }
+    }
+    
+    print(pictures)
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pictures.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+    cell.textLabel?.text = pictures[indexPath.row]
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+      vc.selectedImage = pictures[indexPath.row]
+      vc.row = indexPath.row
+      vc.totalRow = tableView.numberOfRows(inSection: 0)
+      navigationController?.pushViewController(vc, animated: true)
+    }
+  }
+}
+
