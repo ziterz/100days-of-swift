5b1665fce9826e975d58e13515da5a23a43b8518
Ziterz <ziady.mubaraq@gmail.com>
2024-01-02 23:29:58 +0700

    learn: day 25 about refactoring for the win

--- day-024 - day-026/Project4/Project4/ViewController.swift
+++ day-024 - day-026/Project4/Project4/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
   // MARK: Properties
   var webView: WKWebView!
   var progressView: UIProgressView!
+  var websites: [String] = ["apple.com", "ziterz.dev"]
   
   // MARK: Views
   
@@ -40,15 +41,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     
-    let url = URL(string: "https://ziterz.dev")!
+    let url = URL(string: "https://" + websites[1])!
     webView.load(URLRequest(url: url))
     webView.allowsBackForwardNavigationGestures = true
   }
 
   @objc func openTapped() {
     let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
-    ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-    ac.addAction(UIAlertAction(title: "ziterz.com", style: .default, handler: openPage))
+    for website in websites {
+      ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+    }
     ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
     ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
     present(ac, animated: true)
@@ -68,6 +70,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
     if keyPath == "estimatedProgress" {
       progressView.progress = Float(webView.estimatedProgress)
     }
+  }
+  
+  func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+    let url = navigationAction.request.url
+    
+    if let host = url?.host {
+      for website in websites {
+        if host.contains(website) {
+          decisionHandler(.allow)
+          return
+        }
+      }
+    }
+    
+    decisionHandler(.cancel)
   }
 }
 
