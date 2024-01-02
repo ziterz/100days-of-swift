9cc965d579ed994e3e392598cb9f3865a69af4c1
Ziterz <ziady.mubaraq@gmail.com>
2024-01-02 22:02:41 +0700

    learn: day 24 set up project

---
+++ day-024/Project4/Project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Ziady Mubaraq on 02/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
