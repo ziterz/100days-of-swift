c45ddb7134635ded04f1b3954ce966bd33788c18
Ziterz <ziady.mubaraq@gmail.com>
2024-01-03 22:16:44 +0700

    learn: day 28 about lowercased() and indexPath

--- day-027/Project5/Project5/ViewController.swift
+++ day-027/Project5/Project5/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+    
     if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
       if let startWords = try? String(contentsOf: startWordsURL) {
         allWords = startWords.components(separatedBy: "\n")
@@ -33,7 +35,7 @@ class ViewController: UITableViewController {
     tableView.reloadData()
   }
   
-  override func numberOfSections(in tableView: UITableView) -> Int {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return usedWords.count
   }
   
@@ -41,6 +43,48 @@ class ViewController: UITableViewController {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
     cell.textLabel?.text = usedWords[indexPath.row]
     return cell
+  }
+  
+  @objc func promptForAnswer() {
+    let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+    
+    let submitAction = UIAlertAction(title: "Submit", style: .default) {
+      [weak self, weak ac] _ in
+      guard let answer = ac?.textFields?[0].text else { return }
+      self?.submit(answer)
+    }
+    
+    ac.addAction(submitAction)
+    present(ac, animated: true)
+  }
+  
+  func submit(_ answer: String) {
+    let lowerAnswer = answer.lowercased()
+    
+    if isPossible(word: lowerAnswer) {
+      if isOriginal(word: lowerAnswer) {
+        if isReal(word: lowerAnswer) {
+          usedWords.insert(answer, at: 0)
+          
+          let indexPath = IndexPath(row: 0, section: 0)
+          tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+      }
+    }
+    
+  }
+  
+  func isPossible(word: String) -> Bool {
+    return true
+  }
+  
+  func isOriginal(word: String) -> Bool {
+    return true
+  }
+  
+  func isReal(word: String) -> Bool {
+    return true
   }
 }
 
