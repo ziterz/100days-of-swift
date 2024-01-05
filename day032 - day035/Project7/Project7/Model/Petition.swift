f3da4d1230be4f1da218925d447ca5e507b1a1c2
Ziterz <ziady.mubaraq@gmail.com>
2024-01-05 23:09:18 +0700

    learn: day 33 about parsing JSON using the Codable protocal

---
+++ day032 - day035/Project7/Project7/Model/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Ziady Mubaraq on 05/01/24.
+//
+
+import Foundation
+
+struct Petition: Codable {
+  var title: String
+  var body: String
+  var signatureCount: Int
+}
---
+++ day032 - day035/Project7/Project7/Model/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Ziady Mubaraq on 05/01/24.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+  var results: [Petition]
+}
