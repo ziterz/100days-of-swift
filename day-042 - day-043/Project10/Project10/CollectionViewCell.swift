43a4a21f49f1cb9386def65a275df75ebdb4e577
Ziterz <ziady.mubaraq@gmail.com>
2024-01-09 22:59:44 +0700

    learn: day 42 about designing UICollectionView cells

---
+++ day-042 - day-043/Project10/Project10/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Project10
+//
+//  Created by Ziady Mubaraq on 09/01/24.
+//  Copyright © 2024 ziterz.dev. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ day-042 - day-043/Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Ziady Mubaraq on 08/01/24.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
